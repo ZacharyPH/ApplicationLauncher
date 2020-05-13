7696eecabac2e098f547e3c0a2eae630f1b15c08
Zachary Hunt <=>
2020-05-13 15:44:41 -0500

    launch argument unpacking options

--- launcher.py
+++ launcher.py
@@ -1,30 +1,31 @@
-# Apps to launch: MEGA, Slack, PyCharm, Solidworks?
-# Future features: Autolaunch, launch on next startup, single time launch, floating app window / microsevice in bar
 import os
-import csv
 from collections import defaultdict
 
 
-def launch(apps: list):
+def launch(*apps):
     """
     Launches applications
     :param apps: List of apps to be launched
     :return: Dictionary with failures due to invalid or missing paths in paths.csv
     """
     paths = defaultdict(lambda: "")
-    with open("paths.csv", "r") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            paths[row["APPLICATION"]] = row["PATH"]
-    print(paths)
+    with open("paths.csv", "r") as file:
+        for row in file.readlines()[1:]:
+            k, v = row.strip().split(",")
+            paths[k] = v
     fails = {"NoPath": [], "InvalidPath": []}
-
+    if len(apps) == 0:
+        return
+    if not (all(type(app) is str for app in apps) or not type(apps[0]) is list or not type(apps[0]) is set):
+        raise ValueError("*apps must be a list or multiple arguments, but not both")
+    if not type(apps[0]) is str:
+        apps = (*apps,)[0]
     for app in apps:
         path = paths[app.upper()]
         if path == "":
             fails["NoPath"].append(app)
         else:
-            try: 
+            try:
                 os.startfile(path)
             except FileNotFoundError:
                 fails["InvalidPath"].append(app)
@@ -32,18 +33,19 @@ def launch(apps: list):
     return fails
 
 
-def sort_paths(filename="paths.csv"):
+def sort_paths(filename="paths.csv") -> None:
+    """
+    Alphabetizes apps in paths csv
+    :param filename: name of app paths csv file
+    :return: None, except several apps launching
+    """
     paths = {}
-    with open(filename, "r+") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            paths[row["APPLICATION"]] = row["PATH"]
-        csvfile.write("APPLICATION, PATH\n")
-        paths = {k: v for k, v in sorted(paths.items(), key=lambda item: item[0])}
-        writer = csv.writer(csvfile, delimiter=",")
+    with open(filename, "r") as file:
+        for row in file.readlines()[1:]:
+            k, v = row.split(",")
+            paths[k] = v
+    paths = {k: v for k, v in sorted(paths.items(), key=lambda item: item[0])}
+    with open(filename, "w") as file:
+        file.write("APPLICATION, PATH\n")
         for app, path in paths.items():
-            writer.writerow(app + "," + path)
-        # TODO: This doesn't work at all :)
-
-
-sort_paths()
+            file.write(app + "," + path)
